b156a71e18bcc9b9bfb01303babd0b15cd171548
chrischen88 <chrischen2191@gmail.com>
2017-02-27 22:23:15 -0600

    Working on Prob 03

---
+++ src/Prob03.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.io.*;
+
+/**
+ * Created by 18chenc on 2/27/2017.
+ */
+public class Prob03
+{
+    public static void main(String[] args) throws IOException
+    {
+        Scanner in = new Scanner(new File("inputs/prob03.dat"));
+        int sets = new Integer(in.nextLine());
+        while(sets-->0)
+        {
+            String[] arr = in.nextLine().split(",\\s");
+            int largestIndex = 0;
+            for (int i = 0; i < arr.length; i++)
+            {
+                
+            }
+        }
+    }
+}
