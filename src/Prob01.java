54440efb01ec8f5744ae1e74b3bf1935314560dd
chrischen88 <chrischen2191@gmail.com>
2017-02-27 21:35:17 -0600

    Solved Prob #1 "I'm Board"

--- src/Prob01.java
+++ src/Prob01.java
@@ -6,9 +6,23 @@ import java.io.*;
  */
 public class Prob01
 {
-    public static void main(String[] args)
+    public static void main(String[] args) throws IOException
     {
-        System.out.println("Hello World!");
+        Scanner in = new Scanner(new File("inputs/prob01.dat"));
+        int sets = new Integer(in.nextLine());
+        while(sets-->0)
+        {
+            int num = new Integer(in.nextLine());
+            for (int i = 0; i < num; i++)
+            {
+                String s = "";
+                for (int j = 0; j < num; j++)
+                {
+                    s += "# ";
+                }
+                System.out.println(s.trim());
+            }
+        }
 
     }
 }
