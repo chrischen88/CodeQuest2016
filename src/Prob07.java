c51181cfee6471daefd1e7e22fd55e63b5d6dc8c
itscchenboii <pkmntrainerchris@gmail.com>
2017-03-07 08:39:44 -0600

    Finished Prob 06

---
+++ src/Prob07.java
@@ -0,0 +1,19 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+/**
+ * Created by chenc on 3/7/2017.
+ */
+public class Prob07
+{
+    public static void main(String[] args) throws IOException
+    {
+        Scanner in = new Scanner(new File("inputs/prob07.dat"));
+        int sets = new Integer(in.nextLine());
+        while(sets-->0)
+        {
+
+        }
+    }
+}
