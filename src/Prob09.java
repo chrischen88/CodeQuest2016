dfdb2f7347e4d8c135d3731c224bf16189ded8fd
chrischen88 <chrischen2191@gmail.com>
2017-03-09 07:20:08 -0600

    Began Prob09

---
+++ src/Prob09.java
@@ -0,0 +1,14 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+/**
+ * Created by 18chenc on 3/8/2017.
+ */
+public class Prob09
+{
+    public static void main(String[] args)throws IOException
+    {
+        Scanner in = new Scanner(new File("inputs/prob09.dat"));
+    }
+}
